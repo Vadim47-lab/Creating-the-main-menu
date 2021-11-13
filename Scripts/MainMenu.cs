c4f409df8f31413bda69c7a58fe5feb23fa6cb1d
Stepanov Vadim <vadgat777@mail.ru>
2021-11-13 11:28:39 +0700

    first commit

---
+++ Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _startGame;
+    [SerializeField] private GameObject _aboutAuthors;
+    [SerializeField] private GameObject _exitGame;
+    [SerializeField] private Button _start;
+    [SerializeField] private Button _authors;
+    [SerializeField] private Button _exit;
+
+    void Start()
+    {
+        _start.onClick.AddListener(StartGame);
+        _authors.onClick.AddListener(Authors);
+        _exit.onClick.AddListener(Exit);
+    }
+
+    private void StartGame()
+    {
+        _startGame.SetActive(true);
+    }
+
+    private void Authors()
+    {
+        _aboutAuthors.SetActive(true);
+    }
+
+    private void Exit()
+    {
+        _exitGame.SetActive(true);
+    }
+}
